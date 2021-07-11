2d169a62e9c1972f5ce72502dce53ca9d9a6b939
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-07-11 11:02:06 -0300

    Corrigido erro no spress

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const expres = require('express')
+const express = require('express')
 const path = require('path')
-const app = expres()
+const app = express()
 const PORT = process.env.PORT || 4000
-const server = app.listen(PORT, () => console.log('💬 server on port ${PORT}'))
+const server = app.listen(PORT, () => console.log(`💬 server on port ${PORT}`))
 
 app.use(express.static(path.join(__dirname, 'public')))
