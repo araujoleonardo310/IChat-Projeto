ecf06e77e1c9d3662ebdd4ea6fc288b8b249d916
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-07-12 18:34:12 -0300

    adicionados

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const socket = io()
 
+const clientsTotal = document.getElementById('client-total')
+
 socket.on('clients-total', (data) => {
-    console.log(data)
+    clientsTotal.innerText = `Conectados: ${data}`
 })
